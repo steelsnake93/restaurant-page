9af8bf0d68257d0cbc5e7c07cfabfdddb9877584
steelsnake93 <nikolanikotasev@protonmail.com>
2023-02-08 15:05:01 +0100

    Add about module

--- src/aboutTab.js
+++ src/aboutTab.js
@@ -1,8 +1,17 @@
 const aboutTab = () => {
-    const aboutContainer = document.createElement('div');
-    aboutContainer.classList.add('about-container');
+    const about = document.createElement('section');
+    about.classList.add('about');
 
+    const aboutHeader = document.createElement('h2');
+    aboutHeader.classList.add('about-header');
+    aboutHeader.textContent = 'About Our Restaurant';
+    about.appendChild(aboutHeader);
 
-    return aboutContainer;
+    const aboutDescription = document.createElement('p');
+    aboutDescription.classList.add('about-description');
+    aboutDescription.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam posuere, ante in dictum congue, erat ex laoreet nibh, id tempor magna ipsum sit amet enim. Donec tempor justo et nisi condimentum laoreet.';
+    about.appendChild(aboutDescription);
+
+    return about;
 };
 export default aboutTab;
